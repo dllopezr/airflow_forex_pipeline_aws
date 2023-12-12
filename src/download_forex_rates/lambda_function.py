8ed0f381cb26f8ad4682c82f6e471c6c912e5c57
David López <david.lopez@wizeline.com>
2023-12-11 20:37:41 -0500

    Finish lambda function code

--- src/download_forex_rates/lambda_function.py
+++ src/download_forex_rates/lambda_function.py
@@ -1,32 +1,26 @@
 import boto3
 import csv
-from io import BytesIO, StringIO
+from io import StringIO
 import json
 import requests
 
+
+def upload_data(outdata: dict, s3: "botocore.client.S3"):
+
+    bucket_name = "forex-rates-data-david-lopez"
+    base = outdata['base']
+    last_update = outdata['last_update']
+    filename = f"{base}/{last_update}.json"
+    s3.put_object(
+            Bucket=bucket_name,
+            Key=filename,
+            Body=json.dumps(outdata)
+        )
+    print(f"{filename} uploaded")
+
 def lambda_handler(event, context):
-    """
-    BASE_URL = "https://gist.githubusercontent.com/marclamberti/f45f872dea4dfd3eaa015a4a1af4b39b/raw/"
-    ENDPOINTS = {
-        'USD': 'api_forex_exchange_usd.json',
-        'EUR': 'api_forex_exchange_eur.json'
-    }
-    with open('/opt/airflow/dags/files/forex_currencies.csv') as forex_currencies:
-        reader = csv.DictReader(forex_currencies, delimiter=';')
-        for idx, row in enumerate(reader):
-            base = row['base']
-            with_pairs = row['with_pairs'].split(' ')
-            indata = requests.get(f"{BASE_URL}{ENDPOINTS[base]}").json()
-            outdata = {'base': base, 'rates': {}, 'last_update': indata['date']}
-            for pair in with_pairs:
-                outdata['rates'][pair] = indata['rates'][pair]
-            with open('/opt/airflow/dags/files/forex_rates.json', 'a') as outfile:
-                json.dump(outdata, outfile)
-                outfile.write('\n')
-    """
 
     s3 = boto3.client("s3")
-
     BASE_URL = "https://gist.githubusercontent.com/marclamberti/f45f872dea4dfd3eaa015a4a1af4b39b/raw/"
     ENDPOINTS = {
         'USD': 'api_forex_exchange_usd.json',
@@ -40,7 +34,7 @@ def lambda_handler(event, context):
     forex_currencies = forex_currencies['Body'].read().decode('utf-8')
     reader = csv.DictReader(StringIO(forex_currencies), delimiter=';')
 
-    ## Request rates and save in dicts
+    # Request and save rates data in s3
     for idx, row in enumerate(reader):
         base = row['base']
         with_pairs = row['with_pairs'].split(' ')
@@ -48,9 +42,7 @@ def lambda_handler(event, context):
         outdata = {'base': base, 'rates': {}, 'last_update': indata['date']}
         for pair in with_pairs:
             outdata['rates'][pair] = indata['rates'][pair]
-        print(outdata)
-
-lambda_handler('','')
+        upload_data(outdata, s3)
 
 
 
