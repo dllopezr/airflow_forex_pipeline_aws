159196fe62e1588c037008d20cde62c52823d315
David López <david.lopez@wizeline.com>
2023-12-15 19:16:47 -0500

    Pyspark script for the transform_forex_rates glue jobs

--- src/transform_forex_rates/transform_forex_rates.py
+++ src/transform_forex_rates/transform_forex_rates.py
@@ -9,7 +9,7 @@ sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 job = Job(glueContext)
-job.init()
+job.init("glueetl")
 
 input_bucket_name = "forex-rates-data-david-lopez"
 output_bucket_name = "transformed-forex-rates-david-lopez"
@@ -22,26 +22,19 @@ files = [file['Key'] for file in contents]
 
 
 def normalize_df(base_currency, df):
-     # create the base for a normalized dataframe
-    df_normalized = df.select("base", "last_update")
-    df_normalized = df_normalized.withColumn(base_currency, lit(1.0)) 
-    # access the currencies and it's exchange values
-    currencies = df.select(map_keys("rates"))
-    currencies = currencies.first()[0] 
-    for currency in currencies:
-      currency_value = df.select(f"rates.{currency}").first()[0]
-      df_normalized = df_normalized.withColumn(currency, lit(currency_value))
+    df = df.select("base", "last_update", "rates.*")
+    df = df.withColumn(base_currency, lit(1.0))
     
-    return df_normalized
+    return df
 
 def order_columns(df):
     currencies_columns = df.columns[2:]
     sorted_currencies_columns = sorted(currencies_columns)
     df_normalized = df.select("base")
-    for column in sorted_currencies_columns: 
+    for column in sorted_currencies_columns:
       value = df.select(column).first()[0]
-      df = df_normalized.withColumn(column, lit(value))
-    last_update = df_normalized.select("last_update").first()[0]
+      df_normalized = df_normalized.withColumn(column, lit(value))
+    last_update = df.select("last_update").first()[0]
     df_normalized= df_normalized.withColumn("last_update", lit(last_update))
     
     return df_normalized
@@ -52,10 +45,10 @@ for file in files:
     last_update = file.split('/')[1].split('.')[0]
     df = spark.read.json(f"s3a://{input_bucket_name}/{file}")
     df = normalize_df(base_currency, df)
+    df.show(truncate=False)
     df = order_columns(df)
 
     # Save to s3
-    filename = f"{base_currency}_{last_update}.csv"
-    df.write.csv(f"s3a://{output_bucket_name}/{filename}", header=True, mode="overwrite")
+    df.write.csv(f"s3a://{output_bucket_name}/{base_currency}", header=True, mode="overwrite")
 
 job.commit()
